2876899d1e7aec21fd3235dc8c8fd309d1b46bbd
Ekaterina Churgel <angeleko@yandex.ru>
2020-07-31 00:46:50 +0300

    Updates

---
+++ precache-manifest.7500deecaead78aad5b2a608d08a63e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0573909ba5c9becd98f626e3ea6bb839",
+    "url": "/what-to-watch/index.html"
+  },
+  {
+    "revision": "934aed27ff36e33600bf",
+    "url": "/what-to-watch/static/css/2.5a2d863c.chunk.css"
+  },
+  {
+    "revision": "0ed2de6789ae7bbb973e",
+    "url": "/what-to-watch/static/css/main.6b6e9723.chunk.css"
+  },
+  {
+    "revision": "934aed27ff36e33600bf",
+    "url": "/what-to-watch/static/js/2.c9b21003.chunk.js"
+  },
+  {
+    "revision": "37958b17b727d67c0a2f063059df3b6f",
+    "url": "/what-to-watch/static/js/2.c9b21003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed2de6789ae7bbb973e",
+    "url": "/what-to-watch/static/js/main.c1e0f353.chunk.js"
+  },
+  {
+    "revision": "aaf77cc001223397cb70",
+    "url": "/what-to-watch/static/js/runtime-main.29656afd.js"
+  }
+]);
